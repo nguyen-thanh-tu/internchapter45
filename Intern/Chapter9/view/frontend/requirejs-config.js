2ae80be5296be58b8c27fea3ff64518c546ec680
Nguyễn Thanh  Tú <71978734+nguyen-thanh-tu@users.noreply.github.com>
2022-01-26 16:18:47 +0700

    de
    de

--- Intern/Chapter9/view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    config: {
-        mixins: {
-            'Magento_Checkout/js/action/set-shipping-information': {
-                'Intern_Chapter9/js/action/set-shipping-information': true
-            }
-        }
-    }
-};
